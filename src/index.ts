4137a15e95093036d6f7b10e06f48d795ac0ca3f
Ephraim Adda <adda.ephraim@gmail.com>
2024-04-11 20:04:50 +0200

    Update 1.0.5

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
-export async function getWordsFromFile(): Promise<string[]> {
+export async function getArrayOfFrenchWord(): Promise<string[]> {
 
     try {
-        // Lire le contenu du fichier JSON
         const jsonData = await fetch("https://raw.githubusercontent.com/words/an-array-of-french-words/master/index.json")
-        const json = await jsonData.json();
-        // Convertir le JSON en tableau
-        return JSON.parse('"' + json + '"')
+        return await jsonData.json();
     } catch (error) {
         console.error('Erreur lors de la lecture du fichier :', error);
         return [];
     }
 }
-
